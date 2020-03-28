0f0a61ad04f310b0797deee0c58d64a8fee13a71
Volmar Kaipainen <volkaipa@ad.helsinki.fi>
2020-03-28 20:43:35 +0200

    muutoksia

--- KlondikePasianssi/src/main/java/klondikePasianssi/gui/UserInterface.java
+++ KlondikePasianssi/src/main/java/klondikePasianssi/gui/UserInterface.java
@@ -2,6 +2,19 @@
 package klondikePasianssi.gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -9,7 +22,17 @@ public class UserInterface extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("moi");
+        
+        GridPane mainPane = new GridPane();
+        mainPane.setBackground(new Background(new BackgroundFill(Color.GREEN, new CornerRadii(10), new Insets(5, 5, 5, 5))));
+        mainPane.setBorder(new Border(new BorderStroke(Color.BURLYWOOD, 
+            BorderStrokeStyle.SOLID, new CornerRadii (10), new BorderWidths(7, 7, 7, 7))));
+       
+        
+        
+        
+        Scene mainScene = new Scene(mainPane, 1200, 800);      
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
     
