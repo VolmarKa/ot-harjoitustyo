c94e17ac6d0d62d125441e2325669b16c10e4b69
Volmar Kaipainen <volkaipa@ad.helsinki.fi>
2020-03-30 12:10:05 +0300

    muutoksia

--- KlondikePasianssi/src/main/java/klondikePasianssi/gui/UserInterface.java
+++ KlondikePasianssi/src/main/java/klondikePasianssi/gui/UserInterface.java
@@ -1,11 +1,15 @@
 
 package klondikePasianssi.gui;
 
+import klondikePasianssi.logics.Card;
 import javafx.application.Application;
+import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
@@ -28,7 +32,11 @@ public class UserInterface extends Application {
         mainPane.setBorder(new Border(new BorderStroke(Color.BURLYWOOD, 
             BorderStrokeStyle.SOLID, new CornerRadii (10), new BorderWidths(7, 7, 7, 7))));
        
+        ImageView im = new ImageView(new Image("01c.gif"));
+        Button nappi = new Button();
+        nappi.setGraphic(im);
         
+        mainPane.add(nappi, 1, 1);
         
         
         Scene mainScene = new Scene(mainPane, 1200, 800);      
--- KlondikePasianssi/src/main/java/klondikePasianssi/logics/Card.java
+++ KlondikePasianssi/src/main/java/klondikePasianssi/logics/Card.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package klondikePasianssi.logics;
 
-/**
- *
- * @author volma
- */
+import javafx.scene.image.Image;
+
 public class Card {
+
+    public enum Suit {
+        CLUBS, DIAMONDS, SPADES, HEARTS;
+    }
+
+    public enum Rank {
+        
+        ACE(1), DEUCE(2), THREE(3), FOUR(4), 
+        FIVE(5), SIX(6), SEVEN(7), EIGHT(8), 
+        NINE(9), TEN(10), JACK(11), QUEEN(12), 
+        KING(13);
+        
+        private final int rankValue;
+            
+        private Rank(int rankValue){
+            this.rankValue=rankValue;
+        }
+        
+        public int getRankValue(){
+            return this.rankValue;
+        }
+    }
+     
+    private final Suit suit;
+    private final Rank rank;
+    private Image cardImage;
+
+    public Card(Suit suit, Rank rank, Image image) {
+        this.suit = suit;
+        this.rank = rank;
+        this.cardImage = image;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+    
+    public Rank getRank(){
+        return this.rank;
+    }
+    
     
 }
