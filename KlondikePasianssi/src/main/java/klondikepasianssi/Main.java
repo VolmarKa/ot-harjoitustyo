1a624085f1c0ed0a05e00d8eec2b43295157c5a3
Volmar Kaipainen <volkaipa@ad.helsinki.fi>
2020-04-27 11:30:06 +0300

    testi

--- KlondikePasianssi/src/main/java/klondikepasianssi/Main.java
+++ KlondikePasianssi/src/main/java/klondikepasianssi/Main.java
@@ -5,6 +5,7 @@ import klondikepasianssi.gui.UserInterface;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("");
         UserInterface.main(args);
     }
 
